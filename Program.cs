97842047370a6a043371c8a78c6e365d83eff871
Ion <127430807+IonGadarenco@users.noreply.github.com>
2025-04-29 13:37:04 +0300

    Add new books

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ internal class Program
             userRepository.Add(new User { Id = 2, Email = "dana@gmail.com", Name = "Dana" });
 
             bookRepository.Add(new Book { Id = 1, Author = "Mihai Malancea", Title = "Uceniciea in contextul asiatic" });
+            bookRepository.Add(new Book { Id = 2, Author = "Cristian Barbosu", Title = "Biserica Verticala" });
+            bookRepository.Add(new Book { Id = 1, Author = "Ioan Bunaciu", Title = "Misiunea evanghelica in contextul rural" });
 
             foreach (var book in bookRepository.GetAll())
             {
